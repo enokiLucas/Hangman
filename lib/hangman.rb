4219d7e13e4726dfeaf8c5bf2632e0bdf32cc49e
Lucas Enoki <enoki.lucas@gmail.com>
2023-04-09 22:42:47 -0300

    Create a new class and put all the code into it

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,65 +1,77 @@
-file = File.read("../english_words.txt")
-word = File.readlines("../english_words.txt")
+class Hangman
+  def initialize
+    @lives = 5
+    @word = select_word
+    @secret_letters = Array.new(@word.length - 1, "_")
+  end
 
+  def run
+    puts @word
+    display_letters
+    choose_letter
+    display_letters
+    display_lives
+  end
 
-#puts word.is_a?(Array) => true
+  private
 
-=begin
-File.open("words_1.txt", "w") do |file|
-  word.each do |word|
-    clean_word = word.gsub(/\W+/, '') # remove any non-word characters
-    if clean_word.length.between?(5, 12)
-      file.puts clean_word
+  def display_lives
+    puts "You have #{@lives} lives left."
+  end
+
+  def select_word
+    line = rand(1..7557)
+    secret_word = File.readlines("words_1.txt")
+    return secret_word[line]
+  end
+
+  def display_letters
+    @secret_letters.each do |letter|
+      print "#{letter} "
+    end
+  end
+
+  def choose_letter
+    print "Choose a letter: "
+    choosen_letter = gets.chomp
+    test = false
+    @word.chars.each_with_index do |letter, i|
+      if letter == choosen_letter
+        @secret_letters[i] = choosen_letter
+        test = true
+      end
+    end
+    puts test
+    if test == false
+      @lives -= 1
     end
   end
 end
-=end
+
+game = Hangman.new
+game.run
+
 
 =begin
-def display_stickman_with_legs
-  puts "     o      "
-  puts "    /|\\     "
-  puts "  __/_\\__\     "
-  puts "  |     |  "
-  puts "  |     |  "
-  puts "  |     |  "
-end
-
-display_stickman_with_legs
-=end
-
-lives = 5
-def display_lives(lives)
-	puts "You have #{lives}"
-end
-
-def select_word
-	line = rand(1..8450)
-	secret_word = File.readlines("words_1.txt")
-	return secret_word[line]
-end
-
-select_word
-secret_word = select_word
-secret_letter = Array.new(secret_word.length - 1, "_")
-
-def display_letters(secret_letter)
-	secret_letter.each do |letter|
-		print "#{letter} "
-	end
-end
-
-def choose_letter(secret_word, secret_letter)
-	print "Choose a letter: "
-	choosen_letter = gets.chomp
-	secret_word.chars.each_with_index do |letter, i|
-		if letter == choosen_letter
-			secret_letter[i] = choosen_letter
+File.open("words_1.txt", "w") do |file|
+	word.each do |word|
+		clean_word = word.gsub(/\W+/, '') # remove any non-word characters
+		if clean_word.length.between?(5, 12)
+			file.puts clean_word
 		end
 	end
 end
+=end
 
-puts secret_word
-display_letters(secret_letter)
-choose_letter(secret_word, secret_letter)
-display_letters(secret_letter)
+=begin
+	def display_stickman_with_legs
+		puts "     o      "
+		puts "    /|\\     "
+		puts "  __/_\\__\     "
+		puts "  |     |  "
+		puts "  |     |  "
+		puts "  |     |  "
+	end
+
+	display_stickman_with_legs
+=end
